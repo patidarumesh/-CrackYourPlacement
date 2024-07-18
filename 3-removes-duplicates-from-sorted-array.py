68ea5708a52962b0e2f35202ca0099a3e0f18f89
Umesh Patidar <patidarumesh281@gmail.com>
2024-07-18 18:09:20 +0000

    remove duplicates done

---
+++ 3-removes-duplicates-from-sorted-array.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+def removeDuplicates(nums):
+    size = len(nums)
+    i=0
+    while i<=size-2:
+        if nums[i]==nums[i+1]:
+            nums.append('_')
+            nums.pop(i+1)
+            size -=1
+        else:
+            i +=1
+        
+    return size, nums
+
+list = [2,2,2,3,5,67,8,8,7]
+print(removeDuplicates(list))
+
+        
